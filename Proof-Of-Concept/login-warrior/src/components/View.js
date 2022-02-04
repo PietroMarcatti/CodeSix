1dc92df9acd6d548259ca163d0610da87dadb0e7
davidespada99 <spadads@gmail.com>
2022-02-04 16:26:01 +0100

    Update View.js

--- Proof-Of-Concept/login-warrior/src/components/View.js
+++ Proof-Of-Concept/login-warrior/src/components/View.js
@@ -1,94 +1,96 @@
-import React, {useState} from "react";
-import HomeOutlined from '@mui/icons-material/HomeOutlined';
-import UploadFile from '@mui/icons-material/UploadFile';
-import Cached from '@mui/icons-material/Cached';
-import InfoOutlined from '@mui/icons-material/InfoOutlined';
-import ContactSupportOutlined from '@mui/icons-material/ContactSupportOutlined';
+import React from "react";
+import { HomeOutlined, UploadFile, Cached, InfoOutlined, ContactSupportOutlined, ArrowBack } from '@mui/icons-material';
 import { BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import UploadFilePage from "./pages/UploadFilePage"
 import ReloadSessionPage from "./pages/ReloadSessionPage";
 import DocsPage from "./pages/DocsPage";
 import InfoPage from "./pages/InfoPage";
-import SelectDimensions from "./SelectDimensions";
-import ExportSession from "./ExportSession";
-import RemoveFile from "./RemoveFile";
 
+import Scatter from "./Graph/Scatter"
+import Sankey from "./Graph/Sankey";
 
-const View = () => {
+class View extends React.Component{
 
-    let menuItems= ["Homepage", "Carica file", "Ricarica Sessione", "Informazioni", "Manuale" ];
-    let menuIcons= [<HomeOutlined fontSize='large'/>, <UploadFile fontSize='large'/>, <Cached fontSize='large'/>, <InfoOutlined fontSize='large'/>, <ContactSupportOutlined fontSize='large'/>];
+    constructor(props){
+        super(props);
+        this.state = {
+            menuItems: ["Homepage", "Carica file", "Ricarica Sessione", "Informazioni", "Manuale" ],
+            menuIcons: [<HomeOutlined fontSize='large'/>, <UploadFile fontSize='large'/>, <Cached fontSize='large'/>, <InfoOutlined fontSize='large'/>, <ContactSupportOutlined fontSize='large'/>],
+            activeMenuItem: 0,
+            menuCollapsed: 0,
+        }
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
 
-    const [showSelectDims, setShowSelectDims] = useState(false);
-    const [showExportSession, setShowExportSession] = useState(false);
-    const [showRemoveFile, setShowRemoveFile] = useState(false);
+    handleClick(i){
+        this.setState({
+                activeMenuItem:i,
+        })
+        console.log(i);
+    }
 
-    return(
-        <Router >
-            <div id="page-wrapper">
-                <div id="menu-wrapper">
-                    <h1 id='menu-title'><b>Login</b> Warrior</h1>
-                    <nav id="menu">
-                        <menu>
-                        <li className="menu-item">
-                                <NavLink to="/" className={(navData) => (navData.isActive ? 'selected' : '')}>
-                                    {menuIcons[0]}
-                                    {menuItems[0]}
-                                </NavLink>
-                            </li>
-                            <li className="menu-item" >
-                                <NavLink to="/uploadFile" className={(navData) => (navData.isActive ? 'selected' : '')}>
-                                    {menuIcons[1]}
-                                    {menuItems[1]}
-                                </NavLink>
-                            </li>
+    toggleMenu(){
+        this.state.menuCollapsed = !this.state.menuCollapsed;
+    }
+
+    render(){
+        return(
+            <Router>
+                <div id="page-wrapper">
+                    <div id="menu-wrapper">
+                        <h1 id='menu-title'><b>Login</b> Warrior</h1>
+                        <nav id="menu">
+                            <menu>
                             <li className="menu-item">
-                                <NavLink to="/reloadSession" className={(navData) => (navData.isActive ? 'selected' : '')}>
-                                    {menuIcons[2]}
-                                    {menuItems[2]}
-                                </NavLink>
-                            </li>
-                            <li className="menu-item">
-                                <NavLink to="/info" className={(navData) => (navData.isActive ? 'selected' : '')}>
-                                    {menuIcons[3]}
-                                    {menuItems[3]}
-                                </NavLink>
-                            </li>
-                            <li className="menu-item">
-                                <NavLink to="/docs" className={(navData) => (navData.isActive ? 'selected' : '')}>
-                                    {menuIcons[4]}
-                                    {menuItems[4]}
-                                </NavLink>
-                            </li>
-                        </menu>
-                    </nav>
-                    <span id="menu-footer">Realizzato da CodeSix per il progetto di Ingegneria del Software A.A. 2021-2022</span>
+                                    <NavLink to="/" className={(navData) => (navData.isActive ? 'selected' : '')}>
+                                        {this.state.menuIcons[0]}
+                                        {this.state.menuItems[0]}
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item" >
+                                    <NavLink to="/uploadFile" className={(navData) => (navData.isActive ? 'selected' : '')}>
+                                        {this.state.menuIcons[1]}
+                                        {this.state.menuItems[1]}
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink to="/reloadSession" className={(navData) => (navData.isActive ? 'selected' : '')}>
+                                        {this.state.menuIcons[2]}
+                                        {this.state.menuItems[2]}
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink to="/info" className={(navData) => (navData.isActive ? 'selected' : '')}>
+                                        {this.state.menuIcons[3]}
+                                        {this.state.menuItems[3]}
+                                    </NavLink>
+                                </li>
+                                <li className="menu-item">
+                                    <NavLink to="/docs" className={(navData) => (navData.isActive ? 'selected' : '')}>
+                                        {this.state.menuIcons[4]}
+                                        {this.state.menuItems[4]}
+                                    </NavLink>
+                                </li>
+                            </menu>
+                        </nav>
+                        <span id="menu-footer">Realizzato da CodeSix per il progetto di Ingegneria del Software A.A. 2021-2022</span>
+                    </div>
+                    <div id="content-wrapper">
+                        <Routes>
+                            <Route path="/" element={<HomePage />}/>
+                            <Route path="/uploadFile" element={<UploadFilePage/>}/>
+                            <Route path="/reloadSession" element={<ReloadSessionPage />}/>
+                            <Route path="/info" element={<InfoPage/>}/>
+                            <Route path="/docs" element={<DocsPage />}/>
+                            <Route path="/scatter" element={<Scatter />}/>
+                            <Route path="/sankey" element={<Sankey />}/>
+                        </Routes>
+                    </div>
                 </div>
-                <div id="content-wrapper">
-                    <Routes>
-                        <Route 
-                            path="/" 
-                            element={
-                                <HomePage 
-                                    showSelectDims={() => setShowSelectDims(true)} 
-                                    showExportSession={() => setShowExportSession(true)}
-                                    showRemoveFile={() => setShowRemoveFile(true)}
-                                    />
-                                }/>
-                        <Route path="/uploadFile" element={<UploadFilePage/>}/>
-                        <Route path="/reloadSession" element={<ReloadSessionPage />}/>
-                        <Route path="/info" element={<InfoPage/>}/>
-                        <Route path="/docs" element={<DocsPage />}/>
-                    </Routes>
-                    <SelectDimensions show={showSelectDims} onClose={()=>setShowSelectDims(false)} />
-                    <ExportSession show={showExportSession} onClose={()=>setShowExportSession(false)}/>
-                    <RemoveFile show={showRemoveFile} onClose={()=>setShowRemoveFile(false)}/>
-                </div>
-            </div>
-        </Router>
-    );
-    
+            </Router>
+        );
+    }
 }
 
 export default View;
